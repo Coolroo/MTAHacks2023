d66768b95bca3457ec41119a86354953e499e321
Wyatt Stagg <coolrooswag@gmail.com>
2023-03-11 15:40:46 -0400

    add response entities

--- backend/src/main/java/com/mtahacks/recipes/controllers/RestController.java
+++ backend/src/main/java/com/mtahacks/recipes/controllers/RestController.java
@@ -6,6 +6,8 @@ import com.mtahacks.recipes.service.OpenAIService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +20,14 @@ public class RestController {
     OpenAIService service;
     Logger logger = LoggerFactory.getLogger(RestController.class);
     @GetMapping("/getRecipe")
-    public Recipe getRecipe(@RequestBody IncomingRequest body){
+    public ResponseEntity<Recipe> getRecipe(@RequestBody IncomingRequest body){
         try{
 
-            return service.getRecipe(body);
+            return ResponseEntity.ok().body(service.getRecipe(body));
         }
         catch(Exception e){
             e.printStackTrace();
-            return null;
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
